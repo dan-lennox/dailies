66faf2babae23e041e36cb0dd6b61647171e722b
Daniel Lennox <lennox.dan@gmail.com>
2015-11-23 23:13:09 +1100

    Added material ui plugins.

--- App.jsx
+++ App.jsx
@@ -1,5 +1,29 @@
+
+
+var {
+  AppCanvas,
+  AppBar,
+  Styles,
+  RaisedButton,
+  DatePicker
+  } = MUI;
+var { ThemeManager, LightRawTheme } = Styles;
+
+FlowRouter.route('/', {
+  name: 'home',
+  action: function(params) {
+    /* The key 'content' is now a function */
+    ReactLayout.render(App, {
+      content() {
+        return <App />;
+      }
+    });
+  }
+});
+
 // App component - represents the whole app
 App = React.createClass({
+
 
   //This mixin makes the getMeteorData method work
   mixins: [ReactMeteorData],
@@ -11,6 +35,18 @@ App = React.createClass({
     }
   },
 
+  // Material UI code. See https://github.com/mrphu3074/react-material-ui/
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  // Material UI code. See https://github.com/mrphu3074/react-material-ui/
+  getChildContext() {
+      return {
+          muiTheme: ThemeManager.getMuiTheme(LightRawTheme)
+      };
+  },
+
   renderDailies() {
     return this.data.dailies.map((daily) => {
       return <Daily key={daily._id} daily={daily} />;
@@ -19,14 +55,24 @@ App = React.createClass({
 
   render() {
     return (
-      <div className="container">
-        <div>
-          <AccountsUIBootstrapWrapper />
-        </div>
-        <div>
+      <AppCanvas>
+        <AppBar 
+          title="Dailies"
+          // iconElementRight = {
+          //   <MUI.IconMenu iconButtonElement={
+          //     <MUI.IconButton><MUI.MoreVertIcon /></MUI.IconButton>
+          //   }>
+          //     <MUI.MenuItem primaryText="Refresh" />
+          //     <MUI.MenuItem primaryText="Help" />
+          //     <MUI.MenuItem primaryText="Sign out" />
+          //   </MUI.IconMenu>
+          // } 
+        />
+        
+        <div className="container">
           {this.renderDailies()}
-        </div>
-      </div>
+       </div>
+      </AppCanvas>
     );
   }
 });
