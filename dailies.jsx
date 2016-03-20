130b6d9a25a2b93d330265493ee5932a595d2f17
Daniel Lennox <lennox.dan@gmail.com>
2016-03-21 00:14:31 +1100

    Moved flowrouter routes to separate file.

--- dailies.jsx
+++ dailies.jsx
@@ -58,37 +58,3 @@ Images.allow({
     return true;
   }
 });
-
-// Routes.
-// TODO: Work out convention here.. should this be stored in another file?
-
-FlowRouter.route('/', {
-  name: 'home',
-  action: function(params) {
-    //The key 'content' is now a function 
-    ReactLayout.render(App, {
-      content() {
-        return <div><DailiesListing /></div>;
-      }
-    });
-  }
-});
-
-FlowRouter.route('/login', {
-  name: 'login',
-  action: function(params) {
-    //The key 'content' is now a function 
-    ReactLayout.render(App, {
-      content() {
-        return <Accounts.ui.LoginFormSet />
-      }
-    });
-  }
-});
-
-FlowRouter.route('/logout', {
-  triggersEnter: [function(context, redirect) {
-    Meteor.logout();
-    FlowRouter.go('/login');
-  }],
-});
